ed4acabc92bf97340aa91bf81f888c62ee905a79
qeqqe <168533332+qeqqe@users.noreply.github.com>
2026-03-30 22:31:08 +0530

    Add type descriptor

--- src/object/header.rs
+++ src/object/header.rs
@@ -1,35 +1,39 @@
 use std::sync::atomic::{AtomicU8, Ordering};
 
-enum MarkColor {
-    White = 0,
-    Gray = 1,
-    Black = 2,
+use crate::object::type_descriptor::TypeDescriptor;
+
+#[derive(Clone, Copy, PartialEq)]
+#[repr(u8)]
+pub enum MarkColor {
+    White = 0, // not yet reached -> dead after mark phase
+    Grey = 1,  // discovered, children not yet scanned
+    Black = 2, // fully scanned
 }
 
 #[repr(C)]
 pub struct GcHeader {
-    // 0 = white (dead, default),
-    // 1 = gray(children not processed),
-    // 2 = black (fully processed)
     pub mark: AtomicU8,
     pub age: u8,
     pub flag: u8,
     pub _pad: u8,
-    pub type_desc: *const u8, // TODO: make a type descriptor for this
+    pub type_desc: *const TypeDescriptor,
     pub size: u32,
 }
 
 impl GcHeader {
     pub fn object_start(&self) -> *mut u8 {
-        unsafe { (self as *const Self).add(1) as *mut u8 }
+        unsafe { (self as *const GcHeader).add(1) as *mut u8 }
+    }
+
+    pub unsafe fn from_object_ptr(obj: *mut u8) -> *mut GcHeader {
+        (obj as *mut GcHeader).sub(1)
     }
 
     pub fn is_marked(&self) -> bool {
-        let mark = self.mark.load(Ordering::Relaxed);
-        1 == mark || 0 == mark
+        self.mark.load(Ordering::Relaxed) != MarkColor::White as u8
     }
 
-    pub fn mark(&self, color: MarkColor) {
+    pub fn set_mark(&self, color: MarkColor) {
         self.mark.store(color as u8, Ordering::Relaxed);
     }
 
