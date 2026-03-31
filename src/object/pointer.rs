8b54cecd59afac4d74d891cf320ecc03be7f786e
qeqqe <168533332+qeqqe@users.noreply.github.com>
2026-03-31 12:16:27 +0530

    Add descriptions for GcPtr

--- src/object/pointer.rs
+++ src/object/pointer.rs
@@ -2,6 +2,16 @@ use std::marker::PhantomData;
 
 use crate::object::header::GcHeader;
 
+/// This is a `GcHeader` wrapper, While carrying &T we have to manage
+/// the lifetime, we don't have static lifetime for the GC objects
+/// they're only alive till our collector allows it to.
+///
+/// We will use raw pointers, let alone they don't have any type information
+/// due to no rust runtime reflection, `GcPtr<T>` is a `#[repr(transparent)]` wrapper
+/// around `GcHeader`, this adds zero overhead and carries the type T through
+/// `PhantomData`
+///
+/// With this we can write/get `&GcHeader` without casting everywhere.
 #[repr(transparent)]
 pub struct GcPtr<T> {
     ptr: *mut GcHeader,
